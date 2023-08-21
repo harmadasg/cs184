5a6d11301b4aae0a2dc06f65fb37d29da0caebce
Gergely Harmadas <harmadasg@gmail.com>
2023-08-22 00:50:47 +0200

    finish homework 2

---
+++ hw2/Transform.cpp
@@ -0,0 +1,100 @@
+// Transform.cpp: implementation of the Transform class.
+
+
+#include "Transform.h"
+
+
+// Helper rotation function.  Please implement this.  
+mat3 Transform::rotate(const float degrees, const vec3& axis) {
+  float cosine = cos(glm::radians(degrees));
+  float sine = sin(glm::radians(degrees));
+
+  vec3 normalized_axis = glm::normalize(axis);
+  float x = normalized_axis.x;
+  float y = normalized_axis.y;
+  float z = normalized_axis.z;
+
+  mat3 unchanged_component = mat3(cosine);
+  mat3 parallel_component = mat3(x*x, x*y, x*z, x*y, y*y, y*z, x*z, y*z, z*z) * (1 - cosine);
+  mat3 perpendicular_component = mat3(0, z, -y, -z, 0, x, y, -x, 0) * sine;
+
+  return unchanged_component + parallel_component + perpendicular_component;
+}
+
+void Transform::left(float degrees, vec3& eye, vec3& up) {
+  mat3 rotation_matrix = rotate(degrees, up);
+  eye = rotation_matrix * eye;
+  up = rotation_matrix * up;
+}
+
+void Transform::up(float degrees, vec3& eye, vec3& up) {
+  vec3 axis = glm::cross(eye, up);
+  mat3 rotation_matrix = rotate(degrees, axis);
+  eye = rotation_matrix * eye;
+  up = rotation_matrix * up;
+}
+
+mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up) {
+  vec3 w = glm::normalize(eye);
+  vec3 u = glm::normalize(glm::cross(up, w));
+  vec3 v = glm::cross(w, u);
+
+  return mat4(
+    u.x, u.y, u.z, glm::dot(u, -eye),
+    v.x, v.y, v.z, glm::dot(v, -eye),
+    w.x, w.y, w.z, glm::dot(w, -eye),
+    0, 0, 0, 1
+  );
+}
+
+mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar) {
+  float f = 1 / tan(glm::radians(fovy/2));
+  return mat4(
+      f / aspect, 0, 0, 0,
+      0, f, 0, 0,
+      0, 0, (zFar + zNear) / (zNear - zFar), 2 * (zFar * zNear) / (zNear - zFar),
+      0, 0, -1, 0
+  );
+}
+
+mat4 Transform::scale(const float &sx, const float &sy, const float &sz) {
+  return mat4(
+    sx, 0, 0, 0,
+    0, sy, 0, 0,
+    0, 0, sz, 0,
+    0, 0, 0, 1
+  );
+}
+
+mat4 Transform::translate(const float &tx, const float &ty, const float &tz) {
+  return mat4(
+    1, 0, 0, tx,
+    0, 1, 0, ty,
+    0, 0, 1, tz,
+    0, 0, 0, 1
+  );
+}
+
+// To normalize the up direction and construct a coordinate frame.  
+// As discussed in the lecture.  May be relevant to create a properly 
+// orthogonal and normalized up. 
+// This function is provided as a helper, in case you want to use it. 
+// Using this function (in readfile.cpp or display.cpp) is optional.  
+
+vec3 Transform::upvector(const vec3 &up, const vec3 &zvec) {
+  vec3 x = glm::cross(up,zvec); 
+  vec3 y = glm::cross(zvec,x); 
+  vec3 ret = glm::normalize(y); 
+  return ret; 
+}
+
+
+Transform::Transform()
+{
+
+}
+
+Transform::~Transform()
+{
+
+}
